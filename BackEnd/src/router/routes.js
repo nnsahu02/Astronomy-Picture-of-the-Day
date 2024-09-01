ee6ce8e21c12a9aa341a9f93fce5adcce1ec2aff
nnsahu02 <nnsahu2022@gmail.com>
2024-09-01 19:11:34 +0530

    -build coomand an entry api added.

--- BackEnd/src/router/routes.js
+++ BackEnd/src/router/routes.js
@@ -5,6 +5,7 @@ const { astroCreate } = require('../controller/astroConreoller')
 
 // GET API for geting the apod data
 router.get('/apod', astroCreate)
+router.get('/', (req, res) => { res.send("APOD server is up, _V : 2") })
 
 
 module.exports = router
